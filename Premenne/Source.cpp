fc2e70d160bcbd401951b5f8989f443baa9421b1
Štefan Bukovan <91267799+Ayay4@users.noreply.github.com>
2021-11-03 09:14:58 +0100

    Add files via upload

---
+++ Premenne/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int hrac1;
+	int hrac2;
+	cout << "hrac1 zadaj cislo" << endl;
+	cin >> hrac1;
+	cout << "hrac2 zadaj cislo" << endl;
+	cin >> hrac2;
+
+	if (hrac1 == hrac2) {
+		cout << "remiza" << endl;
+
+	}
+	else
+
+
+	
+	
+}
